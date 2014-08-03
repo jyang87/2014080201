e06ca6564043c8d4d68f997b0ed6d725d48e2b69
jyang87 <JiangYang@192.168.2.104>
2014-08-03 22:28:41 +0800

    20140802Done

---
+++ ToDoList/ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by JiangYang on 14-8-2.
+//  Copyright (c) 2014年 edu.self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
